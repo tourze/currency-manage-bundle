7b917b56d9235c2707b46b7d1acfd5900d0afd52
github-ci <you@example.com>
2025-06-11 19:53:41 +0800

    Exception => Throwable

--- src/Command/UpdateCurrencyRateCommand.php
+++ src/Command/UpdateCurrencyRateCommand.php
@@ -28,7 +28,7 @@ class UpdateCurrencyRateCommand extends Command
             $output->writeln("成功记录了 {$result['historyCount']} 条新的历史汇率数据");
 
             return Command::SUCCESS;
-        } catch (\Exception $e) {
+        } catch (\Throwable $e) {
             $output->writeln("<error>汇率同步失败：{$e->getMessage()}</error>");
             return Command::FAILURE;
         }
